7ea77524486d14e634b6be56c6036543f652086c
Del-Martin <martindelfini10@gmail.com>
2024-04-05 11:44:33 +0200

    Agregar comida y precios a una lista

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,30 @@
 
 function calcular() {
-    let n1 = parseFloat(document.getElementById('factura').value);
+    let n1 = parseFloat(document.getElementById('comida').value);
     let n2 = parseFloat(document.getElementById('propina').value);
 
     let total = n1 + ( n1 * (n2/100) );
 
     document.getElementById('total').value = total.toFixed(2);
+}
+
+function completarComida(comida, precio) {
+    document.getElementById('comida').value = comida;
+    document.getElementById('precio').value = precio;
+}
+
+function agregarComida() {
+    let comida = document.getElementById('comida').value;
+    let precio = document.getElementById('precio').value;
+
+    let listaComida = document.getElementById('listaComida');
+
+    var listItem = document.createElement("li");
+    listItem.className = "list-group-item";
+    listItem.textContent = comida + ": " + precio + " €";
+
+    listaComidas.appendChild(listItem);
+a
+    document.getElementById("comida").value = "";
+    document.getElementById("precio").value = "";
 }
